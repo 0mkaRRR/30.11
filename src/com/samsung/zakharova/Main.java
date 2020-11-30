113cdedc314d71a834a649762eafd939de9e0239
0mkaRRR <olataer@gmail.com>
2020-11-30 17:52:32 +0200

    Commit two

---
+++ src/com/samsung/zakharova/Main.java
@@ -0,0 +1,15 @@
+package com.samsung.zakharova;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("p1ts4");
+
+    }
+}
+
+
+
+
+
